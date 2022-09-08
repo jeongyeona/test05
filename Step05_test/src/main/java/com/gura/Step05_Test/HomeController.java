3b31d16a51778812e53340ad1fca690a1bfe1815
JEONGYEON <kim9502071@naver.com>
2022-09-08 21:55:10 +0900

    commit

---
+++ Step05_test/src/main/java/com/gura/Step05_Test/HomeController.java
@@ -0,0 +1,25 @@
+package com.gura.Step05_Test;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/home")
+	private String home() {
+	
+
+		return "home";
+	}
+	
+}
